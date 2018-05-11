0c6ef06b39ad2141a57f8c86a562e093e1ca0f08
alex <alex23031996@yandex.ru>
2018-05-11 16:10:40 +0300

    fixed date display

--- app/src/main/java/com/alex/kudago/model/event/Date.kt
+++ app/src/main/java/com/alex/kudago/model/event/Date.kt
@@ -5,5 +5,5 @@ import com.google.gson.annotations.SerializedName
 /**
  * Created by alex on 09.05.2018.
  */
-data class Date(@SerializedName("start_date") var startDate: String,
-                @SerializedName("end_date") var endDate: String)
+data class Date(@SerializedName("start") var startDate: String,
+                @SerializedName("end") var endDate: String)
--- app/src/main/java/com/alex/kudago/presentations/ui/recyclerView/holders/EventsHolder.kt
+++ app/src/main/java/com/alex/kudago/presentations/ui/recyclerView/holders/EventsHolder.kt
@@ -3,6 +3,7 @@ package com.alex.kudago.presentations.ui.recyclerView.holders
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import com.alex.kudago.model.event.Event
+import com.alex.kudago.utlis.TimeConverter
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_events.view.*
 
@@ -13,11 +14,22 @@ class EventsHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun bind(item: Event) {
         itemView.tvNameEvent.text = item.title
+
         if (item.place != null)
             itemView.tvLocation.text = item.place.address
+        else itemView.tvLocation.visibility = View.GONE
+
         itemView.tvPreview.text = item.description
-        itemView.tvMoney.text = item.price
+
+        if (!item.price.isEmpty())
+            itemView.tvMoney.text = item.price
+        else itemView.tvMoney.visibility = View.GONE
+
         loadPhotoPreview(item.images[0].urlImage)
+
+        var timeConverter = TimeConverter()
+        timeConverter.convertTime(item.dates[0].startDate, item.dates[0].endDate)
+        itemView.tvToday.text = timeConverter.convertTime(item.dates[0].startDate, item.dates[0].endDate)
     }
 
     private fun loadPhotoPreview(url: String) {
